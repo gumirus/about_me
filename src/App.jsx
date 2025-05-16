bcab226091aa745c89e96c78046cca565ccc367b
Ruslan Gumirov <gumirus29@gmail.com>
2025-05-16 10:12:09 +0500

    Fix:Corrections and add useSmoothScroll

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import Skills from './components/Skills'
 import Projects from './components/Projects'
 import Education from './components/Education'
 import Footer from './components/Footer'
+import useSmoothScroll from './components/useSmoothScroll.js'
 
 export default function App() {
+  useSmoothScroll()
   return (
     <div className="body-content">
       <div className="container">
-        <Header />
-        <main className="main-content">
+        <main className="max-w-7xl mx-auto px-6 py-10 text-gray-800 scroll-smooth">
+          <Header />
           <Greetings />
           <Skills />
           <Projects />
